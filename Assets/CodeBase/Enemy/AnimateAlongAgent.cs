bd2ef9d4af6061ee73387cc55840931317e2decc
Mansur-unity <48056243+devseylor@users.noreply.github.com>
2025-05-30 17:50:08 +0500

    add animation for enemy

---
+++ Assets/CodeBase/Enemy/AnimateAlongAgent.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace CodeBase.Enemy
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(EnemyAnimator))]
+    public class AnimateAlongAgent : MonoBehaviour
+    {
+        public const float MinimalVelocity = 0.1f;
+
+        public NavMeshAgent Agent;
+        public EnemyAnimator Animator;
+
+
+        private void Update()
+        {
+            if (ShouldMove())
+                Animator.Move(Agent.velocity.magnitude);
+            else
+                Animator.StopMoving();
+        }
+
+        private bool ShouldMove() => 
+            Agent.velocity.magnitude > MinimalVelocity && Agent.remainingDistance > Agent.radius;
+    }
+}
