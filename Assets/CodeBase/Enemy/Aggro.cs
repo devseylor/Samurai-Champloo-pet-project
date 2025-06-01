2fde8b06036ba9fbedf3723b332bcf0eae91d966
Mansur-unity <48056243+devseylor@users.noreply.github.com>
2025-06-01 12:47:57 +0500

    aggro for enemies

---
+++ Assets/CodeBase/Enemy/Aggro.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    public class Aggro : MonoBehaviour
+    {
+        public TriggerObserver TriggerObserver;
+        public AgentMoveToPlayer Follow;
+
+        public float Cooldown;
+
+        private Coroutine _aggroCoroutine;
+        private bool _hasAggroTarget;
+
+        private void Start()
+        {
+            TriggerObserver.TriggerEnter += TriggerEnter;
+            TriggerObserver.TriggerExit += TriggerExit;
+
+            SwitchFollowOff();
+        }
+
+        private void TriggerEnter(Collider collider)
+        {
+            if (!_hasAggroTarget)
+            {
+                _hasAggroTarget = true;
+                StopAggroCoroutine();
+
+                SwitchFollowOn();
+            }
+        }
+
+        private void TriggerExit(Collider collider)
+        {
+            if (_hasAggroTarget)
+            {
+                _hasAggroTarget = false;
+                _aggroCoroutine = StartCoroutine(SwitchFollowOffAfterCoolDown());
+            }
+        }
+
+        private IEnumerator SwitchFollowOffAfterCoolDown()
+        {
+            yield return new WaitForSeconds(Cooldown);
+
+            SwitchFollowOff();
+        }
+
+        private void StopAggroCoroutine()
+        {
+            if (_aggroCoroutine != null)
+            {
+                StopCoroutine(_aggroCoroutine);
+                _aggroCoroutine = null;
+            }
+        }
+        private void SwitchFollowOn() => 
+            Follow.enabled = true;
+
+        private void SwitchFollowOff() => 
+            Follow.enabled = false;
+    }
+}
