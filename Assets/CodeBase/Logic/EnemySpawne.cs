be62d9e49711d2404bf8b20a1c4e8bc34230fa78
Mansur-unity <48056243+devseylor@users.noreply.github.com>
2025-06-20 20:24:33 +0500

    enemy spawn

---
+++ Assets/CodeBase/Logic/EnemySpawne.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using CodeBase.StaticData;
+using UnityEngine;
+
+namespace CodeBase.Logic
+{
+    public class EnemySpawne : MonoBehaviour
+    {
+        public MonsterTypeId MonsterTypeId;
+
+    }
+}
---
+++ Assets/CodeBase/StaticData/MonsterTypeId.cs
@@ -0,0 +1,8 @@
+namespace CodeBase.StaticData
+{
+    public enum MonsterTypeId
+    {
+        Samurai = 0,
+        BigSamurai = 10,
+    }
+}
